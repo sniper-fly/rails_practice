936cd2015a7f2352bb06e53540690baaeaca2cce
RyoNakai <ryo.nakai@super-studio.jp>
2021-06-25 11:58:22 +0900

    カテゴリー名をenumとハッシュで管理し、get_categoryメソッドで名前を取得できるようにした

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,19 @@
 class Inquiry < ApplicationRecord
+  enum category_kinds: {
+    achieve: 0,
+    text: 1,
+    info_session: 2,
+    others: 3
+  }
+
+  CATEGORY_TITLE = {
+    Inquiry.category_kinds[:achieve] => "Achieve",
+    Inquiry.category_kinds[:text] => "テキスト",
+    Inquiry.category_kinds[:info_session] => "説明会",
+    Inquiry.category_kinds[:others] => "その他"
+  }
+
+  def get_category
+    CATEGORY_TITLE[self.category]
+  end
 end
