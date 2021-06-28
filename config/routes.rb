9433bf4c608e797117b54973c4285606d13b927a
RyoNakai <ryo.nakai@super-studio.jp>
2021-06-28 16:25:44 +0900

    deviceのルーティングをカスタマイズした

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'blogs#index'
   resources :blogs
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
