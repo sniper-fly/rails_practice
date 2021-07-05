7e638ea3b86cab8d83f0e82d89d483ec31c937f6
RyoNakai <ryo.nakai@super-studio.jp>
2021-07-05 11:51:42 +0900

    確認画面で更新したときにリダイレクトが発生してしまうため、最初の画面にリダイレクトするように変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :inquiry, only: [:index, :new, :create]
   post '/inquiry/confirm' => 'inquiry#confirm'
+  get '/inquiry/confirm' => 'inquiry#new'
   # post :confirm
 
 
