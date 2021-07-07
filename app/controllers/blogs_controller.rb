b3d95c26abb2bebbd40f0a3834663fae841fe18c
RyoNakai <ryo.nakai@super-studio.jp>
2021-07-07 18:15:09 +0900

    ページネーションのインストールと実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
-    @blogs = Blog.select(:id, :name, :article, :created_at, :user_id).includes(:user)
+    @blogs = Blog.index_all.page(params[:page])
   end
 
   # GET /blogs/1 or /blogs/1.json
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,5 +2,12 @@ class Blog < ApplicationRecord
   validates :name, presence: true
   validates :article, presence: true, length: { maximum: 140 }
   belongs_to :user
+  paginates_per 10
+
+  scope :index_all, -> {
+    select(:id, :name, :article, :created_at, :user_id)
+      .order(created_at: :asc)
+      .includes(:user)
+  }
 end
 
