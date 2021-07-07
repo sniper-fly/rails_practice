12e9566527411bcb890ac17b268d83bc6dbb95ac
RyoNakai <ryo.nakai@super-studio.jp>
2021-07-07 19:14:42 +0900

    commentテーブルにblogカラムが足りなかったので追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_093838) do
+ActiveRecord::Schema.define(version: 2021_07_07_101337) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2021_07_07_093838) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "blog_id", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -93,5 +95,6 @@ ActiveRecord::Schema.define(version: 2021_07_07_093838) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "blogs", "users"
+  add_foreign_key "comments", "blogs"
   add_foreign_key "comments", "users"
 end
