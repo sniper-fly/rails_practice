3073819e071a1f32cadce35d83293c5f1beea8fc
RyoNakai <ryo.nakai@super-studio.jp>
2021-07-07 12:46:27 +0900

    blogsテーブルにユーザのforeign idカラムを追加してmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_014705) do
+ActiveRecord::Schema.define(version: 2021_07_07_015215) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_06_28_014705) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "yeah"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "inquiries", charset: "utf8mb4", force: :cascade do |t|
@@ -82,4 +84,5 @@ ActiveRecord::Schema.define(version: 2021_06_28_014705) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "blogs", "users"
 end
