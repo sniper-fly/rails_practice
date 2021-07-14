9fcf650943d4b8e3ca9c5590b5849938ca638a7d
RyoNakai <ryo.nakai@super-studio.jp>
2021-07-14 10:31:00 +0900

    favoriteテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_101337) do
+ActiveRecord::Schema.define(version: 2021_07_14_012559) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(version: 2021_07_07_101337) do
     t.bigint "blog_id", null: false
     t.index ["blog_id"], name: "index_comments_on_blog_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", charset: "utf8mb4", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "blog_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "inquiries", charset: "utf8mb4", force: :cascade do |t|
