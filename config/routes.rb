637fc9b8497044468973c5eedbc8d2604a39ce36
RyoNakai <ryo.nakai@super-studio.jp>
2021-07-14 18:35:12 +0900

    ルーティングを整理し、user_listにfollow,unfollowできるようなアクションのパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  # devise_for :users
   devise_for :users, controllers: { registrations: 'users/registrations' }
+
   root to: 'blogs#top'
   resources :blogs
   get '/blog/favorites' => 'blogs#favorites'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   resources :inquiry, only: [:index, :new, :create]
-  resources :user_list, only: [:index, :show]
   post '/inquiry/confirm' => 'inquiry#confirm'
   get '/inquiry/confirm' => 'inquiry#new'
+
+  resources :user_list, only: [:index, :show, :create, :destroy]
+  get '/user_list/followers' => 'user_list#followers'
+  get '/user_list/follow' => 'user_list#follow'
+
   resources :comment, only: [:create, :edit, :update, :destroy]
+
   resources :favorites, only: [:create, :destroy]
-  # post :confirm
 
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
